9f9b8fad5f3416f67059dac18e05d78f3c8e62a2
chotunabhi <chotu.n.abhi@gmail.com>
2017-02-12 09:43:00 +0530

    Added last number fix

--- src/com/miscellaneous/Mode.java
+++ src/com/miscellaneous/Mode.java
@@ -4,7 +4,9 @@ import java.util.Arrays;
 
 public class Mode {
 	public static void main(String args[]) {
-		int[] a = { 1, 1, 1, 3, 3, 3, 3, 5, 5, 5, 5, 6, 6, 8, 8 };
+		int[] a = {1,2,3,4,4,5,5}; 
+			//{ 1, 1, 1, 3, 3, 3, 3, 5, 5, 5, 5, 6, 6, 8, 8, 8, 8, 8, 2, 2, 8, 2, 2 };
+
 		Arrays.sort(a);
 
 		System.out.println(findMode(a));
@@ -12,7 +14,7 @@ public class Mode {
 	}
 
 	private static int findMode(int[] a) {
-		int temp1, temp2;
+		int temp1;
 		int count1 = 1, count2 = 1;
 
 		if (a.length == 0)
@@ -21,23 +23,25 @@ public class Mode {
 		if (a.length == 1)
 			return a[0];
 
-		temp1 = temp2 = a[0];
+		temp1 = a[0];
 
-		for (int i = 1; i < a.length; i++) {
+		for (int i = 1; i <= a.length; i++) {
 
-			if (temp2 == a[i])
+			if (i != a.length && a[i - 1] == a[i]) {
 				count2++;
+			}
+
 			else {
 				if (count2 > count1) {
-					temp1 = temp2;
+					temp1 = a[i - 1];
 					count1 = count2;
 				}
 
-				temp2 = a[i];
 				count2 = 1;
 			}
 		}
 
 		return temp1;
 	}
+
 }
