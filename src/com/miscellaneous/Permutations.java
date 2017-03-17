1707b4b7a55ddbccc57c42b4c602cc834f3b720a
abhi <abhi@ABHI>
2017-03-17 11:40:51 +0530

    added permutations logic

--- src/com/miscellaneous/Permutations.java
+++ src/com/miscellaneous/Permutations.java
@@ -4,36 +4,30 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Permutations {
-	public static Set<String> permutationFinder(String str) {
-        Set<String> perm = new HashSet<String>();
-        //Handling error scenarios
-        if (str == null) {
-            return null;
-        } else if (str.length() == 0) {
-            perm.add("");
-            return perm;
-        }
-        char initial = str.charAt(0); // first character
-        String rem = str.substring(1); // Full string without first character
-        Set<String> words = permutationFinder(rem);
-        for (String strNew : words) {
-            for (int i = 0;i<=strNew.length();i++){
-                perm.add(charInsert(strNew, initial, i));
-            }
-        }
-        return perm;
-    }
+	static HashSet<String> permutations = new HashSet<>();
+	
+	private static void permutation(String prefix, String str) {
+	    int n = str.length();
+	    if (n == 0) permutations.add(prefix);
+	    else {
+	        for (int i = 0; i < n; i++)
+	            permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
+	    }
+	}
+	
+	public static void main(String[] args) {
+		String s1 = "ABC";
+		String s2 = "ABCD";
+		
+		permutations.clear();
+		permutation("",s1);
+		System.out.println(permutations);
+		System.out.println(permutations.size());
+		
+		permutations.clear();
+		permutation("",s2);
+		System.out.println(permutations);
+		System.out.println(permutations.size());
 
-    public static String charInsert(String str, char c, int j) {
-        String begin = str.substring(0, j);
-        String end = str.substring(j);
-        return begin + c + end;
-    }
-
-    public static void main(String[] args) {
-        String s1 = "ABC";
-        String s2 = "ABCD";
-        System.out.println("\nPermutations for " + s1 + " are: \n" + permutationFinder(s1));
-        System.out.println("\nPermutations for " + s2 + " are: \n" + permutationFinder(s2));
-    }
+	}
 }
