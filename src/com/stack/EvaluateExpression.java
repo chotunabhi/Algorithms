197d59b4d706e154c91ac93a683d0ad520722f95
Abhi <Abhi@II01-FRQSRF2.ciqhyd.com>
2017-07-07 11:58:03 +0530

    added Evaluate exp skeleton

--- src/com/stack/EvaluateExpression.java
+++ src/com/stack/EvaluateExpression.java
@@ -4,16 +4,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EvaluateExpression {
-	private static Stack<Double> argumentStack = new Stack<>();
-	private static Stack<Character> operatorStack = new Stack<>();
 	private static final Character[] validChars = { '+', '-', '*', '/', '(', ')' };
 	private static final List<Character> validOperators = Arrays.asList(validChars);
+	private static LinkedList<Character> operators = new LinkedList<>();
 
 	public static void main(String[] args) throws IOException {
 		System.out.println("Enter the expression to evaluate : ");
@@ -26,58 +26,29 @@ public class EvaluateExpression {
 
 	private static Double evaluateExpression(String expression) {
 		Double result = 0.0;
-		Pattern pattern = Pattern.compile("((\\d+)([-\\*\\/\\+\\(\\)]*))");
+		Pattern pattern = Pattern.compile("(\\(*)((\\d+)([-\\*\\/\\+\\(\\)]*))");
 		Matcher matcher = pattern.matcher(expression);
 
 		while (matcher.find()) {
-			//			System.out.println(matcher.group() + "->" + matcher.group(2) + " expression " + matcher.group(3));
-			Double argument = Double.parseDouble(matcher.group(2));
-			String operators = matcher.group(3);
+			String startParanthesis = matcher.group(1);
+			System.out.println(matcher.group() + "->" + startParanthesis+" argument " + matcher.group(3) + " expression " + matcher.group(4));
+
+			Double argument = Double.parseDouble(matcher.group(3));
+			String operators = matcher.group(4);
 
 			if (!isValidOperatorContinuation(operators)) {
 				System.err.println("Invalid Expression!" + operators);
 				System.exit(0);
 			}
 
-			argumentStack.push(argument);
-
 			for (int i = 0; i < operators.length(); i++) {
-
 				char currentOperator = operators.charAt(i);
 
-				if (currentOperator == ')') {
-					while (operatorStack.peek() != '(')
-						performOperation();
-
-					operatorStack.pop();
-				}else if(currentOperator == '(')
-					operatorStack.push(currentOperator);
-				else {
-					while (!operatorStack.isEmpty()
-							&& (operatorPriority(operatorStack.peek()) > operatorPriority(currentOperator)))
-						performOperation();
-
-					operatorStack.push(currentOperator);
-				}
 			}
 		}
 
-		while(!operatorStack.isEmpty())
-			performOperation();
-		return argumentStack.pop();
-	}
-
-	private static void performOperation() {
-		Double result;
-		double operand1 = argumentStack.pop();
-		double operand2 = argumentStack.pop();
-		char operator = operatorStack.pop();
-
-		result = operation(operand2, operand1, operator);
-
-		argumentStack.push(result);
-		System.out.println(argumentStack);
-		System.out.println(operatorStack);
+		
+		return null;
 	}
 
 	private static Double operation(double operand1, double operand2, char operator) {
