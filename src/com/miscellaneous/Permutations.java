b85b4a8d02875aab40c120fb25db97556860f07b
abks <abks@ABKS-LAP.oradev.oraclecorp.com>
2019-07-21 01:01:01 +0530

    Modified Permutations method

--- src/com/miscellaneous/Permutations.java
+++ src/com/miscellaneous/Permutations.java
@@ -1,10 +1,10 @@
 package com.miscellaneous;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Permutations {
-	static HashSet<String> permutations = new HashSet<>();
+	static List<String> permutations = null;
 	
 	private static void permutation(String prefix, String str) {
 	    int n = str.length();
@@ -19,15 +19,19 @@ public class Permutations {
 		String s1 = "ABC";
 		String s2 = "ABCD";
 		
-		permutations.clear();
+		permutations = new ArrayList<>(fact(s1.length()));
 		permutation("",s1);
 		System.out.println(permutations);
 		System.out.println(permutations.size());
 		
-		permutations.clear();
+		permutations = new ArrayList<>(fact(s2.length()));
 		permutation("",s2);
 		System.out.println(permutations);
 		System.out.println(permutations.size());
 
+	}
+
+	private static int fact(int length) {
+		return (length ==0)? 1 : (length * fact(length -1));
 	}
 }
