86404f0c8096fa4e1d83d6f829ddae6079788fdb
abks <abks@192.168.1.8>
2021-08-12 21:23:51 +0530

    Added parameterized junits

--- test/com/dsnalgomadeeasy/linkedList/problems/MiddleOfSLLTest.java
+++ test/com/dsnalgomadeeasy/linkedList/problems/MiddleOfSLLTest.java
@@ -1,53 +1,69 @@
 package com.dsnalgomadeeasy.linkedList.problems;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import com.dsnalgomadeeasy.linkedList.common.NodeSLL;
 import com.dsnalgomadeeasy.linkedList.common.SLLUtil;
 
-public class MiddleOfSLLTest {
-	MiddleOfSLL middleOfSLL = new MiddleOfSLL();
+@RunWith(Parameterized.class)
+public  class MiddleOfSLLTest{
+	private MiddleOfSLL middleOfSLL;
+	private NodeSLL<Integer> expectedResult;
+	private NodeSLL<Integer> head;
 	
-	@Test
-	public void testMiddleOfSLLNullList() {
-		testImplementationMethods(null, null);
+	public MiddleOfSLLTest(NodeSLL<Integer> expectedResult, NodeSLL<Integer> head) {
+		this.expectedResult = expectedResult;
+		this.head = head;
 	}
 	
-	@Test
-	public void testMiddleOfSLLSingleNode() {
-		NodeSLL<Integer> head = new NodeSLL<Integer>(1);
+	@Before
+	public void init() {
+		middleOfSLL = new MiddleOfSLL();
+	}
+	
+	@Parameterized.Parameters
+	public static Collection listData() {
+		NodeSLL<Integer> singleNode = new NodeSLL<Integer>(1);
+		NodeSLL<Integer> twoNode = SLLUtil.createSLL(new Integer[] {1,2});
+		NodeSLL<Integer> oddNode = SLLUtil.createSLL(new Integer[] {1,2,3,4,5});
+		NodeSLL<Integer> evenNode = SLLUtil.createSLL(new Integer[] {1,2,3,4});
 		
-		testImplementationMethods(head, head);
+		return Arrays.asList(new Object[][] {
+			{null,null},
+			{twoNode,twoNode},
+			{oddNode.getNext().getNext(),oddNode},
+			{evenNode.getNext(),evenNode}
+		});
 	}
 	
 	@Test
-	public void testMiddleOfSLLTwoNodes() {
-		NodeSLL<Integer> head = SLLUtil.createSLL(new Integer[] {1,2});
-		
-		testImplementationMethods(head, head);
-	}
-	
-	@Test
-	public void testMiddleOfSLLOddNodes() {
-		NodeSLL<Integer> head = SLLUtil.createSLL(new Integer[] {1,2,3,4,5});
-		
-		testImplementationMethods(head, head.getNext().getNext());
-	}
-	
-	@Test
-	public void testMiddleOfSLLEvenNodes() {
-		NodeSLL<Integer> head = SLLUtil.createSLL(new Integer[] {1,2,3,4});
-		
-		testImplementationMethods(head, head.getNext());
-	}
-	
-	private <T extends Comparable<T>> void testImplementationMethods(NodeSLL<T> head,NodeSLL<T> expectedResult) {
+	public void testMiddleOfSLLBruteForce() {
 		assertEquals(expectedResult, middleOfSLL.middleOfSLLBruteForce(head));
+	}
+	
+	@Test
+	public void testMiddleOfSLLByFindingLength() {
 		assertEquals(expectedResult, middleOfSLL.middleOfSLLByFindingLength(head));
-		assertEquals(expectedResult, middleOfSLL.middleOfSLLUsingHashTable(head));
-		assertEquals(expectedResult, middleOfSLL.middleOfSLLON(head));
 	}
 
+	@Test
+	public void testMiddleOfSLLUsingHashTable() {
+		assertEquals(expectedResult, middleOfSLL.middleOfSLLUsingHashTable(head));
+	}
+	
+	@Test
+	public void testMiddleOfSLLON() {
+		assertEquals(expectedResult, middleOfSLL.middleOfSLLON(head));
+	}
 }
