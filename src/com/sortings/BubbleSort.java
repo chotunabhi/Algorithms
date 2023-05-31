79ad25054588646da83883588e7fbde4517722d6
abhilashkumar.ks <abhilashkumar.ks@servicenow.com>
2023-05-31 20:32:50 +0530

    Re written Bubble sort

--- src/com/sortings/BubbleSort.java
+++ src/com/sortings/BubbleSort.java
@@ -4,20 +4,20 @@ public class BubbleSort implements ISort {
 
 	@Override
 	public <T extends Comparable<T>> T[] sort(T[] a) {
-		// TODO Auto-generated method stub
-		for(int i=0;i<a.length;i++){
-			int count=0;
-			for(int j=i;j<a.length - 1;j++){
-				if(a[j].compareTo(a[j+1])> 0 )
-				{
-					T temp = a[j];
-					a[j] = a[j+1];
-					a[j+1] = temp;
+		for(int i = a.length; i > 0; i--) {
+			boolean swapped = false;
+			
+			for(int j = 0 ; j < i - 1; j++) {
+				if(a[j+1].compareTo(a[j]) < 0) {
+					T temp = a[j+1];
+					a[j+1] = a[j];
+					a[j] = temp;
+					
+					swapped = true;
 				}
-				else
-					count++;
 			}
-			if(count == a.length)
+			
+			if(!swapped)
 				break;
 		}
 		
